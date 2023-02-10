e2adf3db860185dc8bc6c39c5c5bc0311e2e932e
Gabriel Bridges <gabrielb71@gmail.com>
2023-02-10 10:48:49 -0800

    changed user input method to argparse

--- python_script_CLI/main.py
+++ python_script_CLI/main.py
@@ -1,3 +1,4 @@
+import argparse
 import os as os
 import numpy as np
 import pandas as pd
@@ -10,7 +11,18 @@ from charge_scan import charge_scan
 from threshold_scan import threshold_scan
 from compute_par_inj import get_parasitic_injection
 
+parser = argparse.ArgumentParser(prog = 'chargescan', description = 'Analyzes Charge Scan Data from GAPS Tracker')
+parser.add_argument('inFile', help = 'filepath to charge scan file')
+parser.add_argument('outPath', help = 'filepath to output directory')
+parser.add_argument('-c', '--minChan', type = int, default = 0, help = 'minimum channel to analyze')
+parser.add_argument('-C', '--maxChan', type = int, default = 31, help = 'maximum channel to analyze')
+parser.add_argument('-e', '--exclude', type = int, nargs='*', help = 'channels to exclude')
+parser.add_argument('-p', '--parasitic', action = 'store_true', help = 'compensate for parasitic capacitance')
+parser.add_argument('-P', '--pedestal', help = 'filepath to pedestal data (necessary for parastitic capacitance correction)')
+parser.add_argument('-T', '--transferFunc', help = 'filepath to transfer function data (necessary for parastitic capacitance correction)')
+parser.add_argument('-t', '--peakingTime', type = int, help = 'peaking time')
 
+<<<<<<< HEAD
 while True:
     print("\n*** GAPS CHARGE SCAN TOOL v1.2 ***\n")
 
@@ -57,11 +69,34 @@ while True:
 
         else:
             excl_channels = []
+=======
+args = parser.parse_args()
 
-    output_folder_filepath = input("               Output folder filepath: ")
-    if output_folder_filepath[0] == '"':
-        output_folder_filepath = output_folder_filepath.replace('"', "")
+# LaTex interpreter
+plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 
+# PLOT CONFIGURATION
+# Label size
+matplotlib.rcParams["axes.labelsize"] = 13
+# Tick label size
+matplotlib.rcParams["xtick.labelsize"] = 13
+matplotlib.rcParams["ytick.labelsize"] = 13
+# Figure size
+matplotlib.rcParams["figure.figsize"] = 6.4 * 1.5, 4.8 * 1.5
+# Legend font size
+matplotlib.rcParams["legend.fontsize"] = 10
+>>>>>>> fcef162 (changed user input method to argparse)
+
+# Read data from file
+data = pd.read_csv(
+    args.inFile,
+    comment="#",
+    sep="\t",
+    header=None,
+)
+data_bkp = data
+
+<<<<<<< HEAD
     # Ask threshold for channel deactivation
     deactivate_thr = input("      Deactivate channels below [keV]: ")
     if deactivate_thr != "":
@@ -75,31 +110,46 @@ while True:
         deactivate_enc = int(deactivate_enc)
     else:
         deactivate_enc = 500
+=======
+# Configuration
+conv_factor = 0.841
+channels = range(args.minCh, args.maxCh + 1)
+channels = np.setdiff1d(channels, args.exclude)
 
-    # Read data from file
-    data = pd.read_csv(
-        filename_chargescan,
-        comment="#",
-        sep="\t",
-        header=None,
-    )
-    data_bkp = data
+# Determine if charge scan or threshold scan
+n_events = data.iloc[0][2]
+threshold_col = data.iloc[:, 0].to_numpy()
+thr_unique = np.unique(threshold_col)
+charge_scan_flag = True
+>>>>>>> fcef162 (changed user input method to argparse)
 
-    # Configuration
-    conv_factor = 0.841
-    channels = range(ch_min, ch_max + 1)
-    channels = np.setdiff1d(channels, excl_channels)
+if len(thr_unique) > 1:
+    charge_scan_flag = False
 
-    # Determine if charge scan or threshold scan
-    n_events = data.iloc[0][2]
-    threshold_col = data.iloc[:, 0]
-    threshold_col = threshold_col.to_numpy()
-    thr_unique = np.unique(threshold_col)
-    charge_scan_flag = True
+if charge_scan_flag:
+    # Charge scan without removal of parasitic injection
+    # Always done
+    (xmin, xmax) = charge_scan(data, channels, conv_factor, args.outPath)
 
-    if len(thr_unique) > 1:
-        charge_scan_flag = False
+    if args.parasitic:
+        if args.pedestal is None or args.transferFunc is None or args.peakingTime is None:
+            raise(FileExistsError('Need pedestal, and transfer function data for parasitic injection compensation'))
+        allch_par_inj_estimate = []
+        for ch in channels:
+            ch_par_inj_estimate = get_parasitic_injection(args.pedestal, args.transferFunc, ch, args.peakingTime)
+            allch_par_inj_estimate.append(ch_par_inj_estimate)
 
+        # Charge scan with subtracted parasitic injection
+        charge_scan_noinj(
+            data,
+            channels,
+            conv_factor,
+            args.outPath,
+            xmin,
+            xmax,
+        )
+
+<<<<<<< HEAD
     if charge_scan_flag:
         # Ask user to compensate parasitic injection or not
         comp_parinj_check = False
@@ -187,3 +237,7 @@ while True:
     else:
         # Threshold scan
         threshold_scan(data_bkp, channels, n_events, output_folder_filepath)
+=======
+else: # Threshold scan
+    threshold_scan(data_bkp, channels, n_events, output_folder_filepath)
+>>>>>>> fcef162 (changed user input method to argparse)
